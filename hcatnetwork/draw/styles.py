5b7c6504c0f72526a71a1ed1cb2a38ffb94a1ef3
Matteoleccardi <leccardi.matteo@gmail.com>
2023-11-14 15:17:36 +0100

    Code made independent from PyQt6 for Google Colab

--- hcatnetwork/draw/styles.py
+++ hcatnetwork/draw/styles.py
@@ -6,14 +6,17 @@ matplotlib
 palettable (https://jiffyclub.github.io/palettable/)
 """
 import matplotlib
-import PyQt6.QtGui
+#import PyQt6.QtGui
 
 def get_primary_screen_dpi():
     """Get the DPI of the primary screen"""
+    """
     app = PyQt6.QtGui.QGuiApplication([])
     screen = app.primaryScreen()
     dpi = screen.physicalDotsPerInch()
     return dpi
+    """
+    return 96
 
 MILLIMETERS_TO_INCHES = 0.0393701
 INCHES_TO_MILLIMETERS = 1.0 / MILLIMETERS_TO_INCHES
