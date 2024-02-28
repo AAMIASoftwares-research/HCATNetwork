4d8f392108b41fd98e2cdc903bfbbb7ef303ad54
Matteoleccardi <leccardi.matteo@gmail.com>
2024-02-28 14:32:24 +0100

    SimpleCenterlineGraph: added the artists to plot the edges and also the circles of the nodes in 2d and 3d

--- hcatnetwork/graph/graph.py
+++ hcatnetwork/graph/graph.py
@@ -21,6 +21,7 @@ and use: python -m hcatnetwork.graph.graph
 from __future__ import annotations
 
 from enum import Enum, auto
+import random
 import numpy
 import matplotlib, mpl_toolkits
 import networkx
@@ -28,6 +29,7 @@ import networkx
 from ..core.core import CoreDict, TYPE_NAME_TO_TYPE_DICT
 from ..node.node import SimpleCenterlineNodeAttributes, ArteryNodeTopology, ArteryNodeSide
 from ..edge.edge import SimpleCenterlineEdgeAttributes
+from ..geometry.reference_frames import get_orthogonal_axes, transform_to_reference_frame, transform_from_reference_frame
 
 ###################################
 # LOADING and SAVING to text files
@@ -525,6 +527,48 @@ class SimpleCenterlineGraph(networkx.classes.graph.Graph):
                         edge_features.update_weight_from_euclidean_distance()
                         graph_new.add_edge(n0, n1, **edge_features)
         return graph_new
+
+    def get_flow_direction(self, node_id: str) -> numpy.ndarray:
+        """Gets the direction towards the next node, where the next node is the distal connected node.
+        If an endpoint, the direction from the previous node to the endpoint is returned.
+        If a bifurcation, the direction towards the next node is the average of the directions towards the next nodes.
+        """
+        node = self.nodes[node_id]
+        ostium = self.get_relative_coronary_ostia_node_id(node_id)[0]
+        node_distance_from_ostium = networkx.algorithms.shortest_path_length(self, ostium, node_id, weight="euclidean_distance")
+        node_neighbors = list(self.neighbors(node_id))
+        node_neighbors_distance_from_ostium = numpy.array([
+            networkx.algorithms.shortest_path_length(self, ostium, n, weight="euclidean_distance")
+            for n in node_neighbors
+        ])
+        node_neighbours_flow = node_neighbors_distance_from_ostium - node_distance_from_ostium
+        node_position = numpy.array([node["x"], node["y"], node["z"]])
+        node_neighbors_positions = [
+            numpy.array([self.nodes[n]["x"], self.nodes[n]["y"], self.nodes[n]["z"]])
+            for n in node_neighbors
+        ]
+        # every node except endpoints - bifurcations are automatically handled
+        if (node_neighbours_flow > 0).any():
+            # net the next neighbour(s)
+            next_neighbours = [i for i in range(len(node_neighbors)) if node_neighbours_flow[i] > 0]
+            # get the direction towards the next node
+            direction = numpy.array([0.0, 0.0, 0.0])
+            for next_n_idx in next_neighbours:
+                d = node_neighbors_positions[next_n_idx] - node_position
+                direction += d / numpy.linalg.norm(d)
+            direction /= len(next_neighbours)
+        # endpoints - bifurcations are automatically handled
+        elif (node_neighbours_flow < 0).any():
+            # get the direction from the previous node(s) to the endpoint
+            previous_neighbours = [i for i in range(len(node_neighbors)) if node_neighbours_flow[i] < 0]
+            direction = numpy.array([0.0, 0.0, 0.0])
+            for previous_n_idx in previous_neighbours:
+                d = node_position - node_neighbors_positions[previous_n_idx]
+                direction += d / numpy.linalg.norm(d)
+            direction /= len(previous_neighbours)
+        else:
+            raise RuntimeError(f"Node \"{node_id}\" has neighbours neither closest or furthest from the related ostium.")
+        return direction
     
     def get_edge_artist_2d(self, reference_system: tuple[str, str] = ('x','y'), **kwargs) -> matplotlib.collections.LineCollection:
         """Gets the edge artist for 2D plotting.
@@ -565,6 +609,62 @@ class SimpleCenterlineGraph(networkx.classes.graph.Graph):
         artist = matplotlib.collections.LineCollection(edge_coordinates, **kwargs)
         return artist
     
+    def get_node_artist_2d_circles(self, reference_system: tuple[str,str] = ('x', 'y'), n_sides: int = 8, prevalence: float=1.0, **kwargs) -> matplotlib.collections.LineCollection:
+        """Gets the node artist for 2D plotting as a matplotlib.collections.LineCollection.
+        Add it to the axes with ax_2d.add_collection(artist).
+
+        Parameters
+        ----------
+        reference_system : tuple, optional
+            The reference system to use for plotting, by default ('x','y').
+            Available: ['x', 'y', 'z']. The first element is the x-axis, the second element is the y-axis.
+        n_sides : int, optional
+            The number of sides to use for each circle (approximation of circle), by default 8.
+        prevalence : float, optional
+            The prevalence of the circles, by default 1.0. The probability that a node will have a circle drawn around it.
+        **kwargs
+            Arbitrary keyword arguments to be passed to the matplotlib.collections.LineCollection constructor.
+        
+        Returns
+        -------
+        matplotlib.collections.LineCollection
+            The node artist for 2D plotting.
+        """
+        # Clean input
+        if len(reference_system) != 2:
+            raise ValueError(f"reference_system must be a 2-tuple, not {reference_system}.")
+        if not all([isinstance(i, str) for i in reference_system]):
+            raise ValueError(f"reference_system must contain char/string, not {reference_system}.")
+        if not all([i in ['x','y','z'] for i in reference_system]):
+            raise ValueError(f"reference_system must contain char/string in ['x','y','z']), not {reference_system}.")
+        if reference_system[0] == reference_system[1]:
+            raise ValueError(f"reference_system must contain two different elements, not {reference_system}.")
+        # Manage prevalence
+        prevalence_vector = []
+        c_ = 0
+        for i in range(len(self.nodes)):
+            if c_*prevalence >= 1:
+                c_ = 0
+                prevalence_vector.append(True)
+            else:
+                prevalence_vector.append(False)
+                c_ += 1
+        nodes_to_process = [n for i, n in enumerate(self.nodes) if prevalence_vector[i]]
+        # Get the node coordinates
+        node_coordinates = [numpy.array([self.nodes[n][reference_system[0]], self.nodes[n][reference_system[1]]]) for n in nodes_to_process]
+        node_radii = [self.nodes[n]["r"] for n in nodes_to_process]
+        thetas = numpy.linspace(0, 2*numpy.pi, n_sides+1)
+        thetas = numpy.append(thetas, thetas[0])
+        circles_list = [
+            [
+                [nc[0]+nr*numpy.cos(th), nc[1]+nr*numpy.sin(th)]
+                for th in thetas
+            ] for nc, nr in zip(node_coordinates, node_radii)
+        ]
+        # Create the artist
+        artist = matplotlib.collections.LineCollection(circles_list, **kwargs)
+        return artist
+    
     def get_edge_artist_3d(self, **kwargs) -> mpl_toolkits.mplot3d.art3d.Line3DCollection:
         """Gets the edge artist for 3D plotting.
         Add it to the axes with ax_3d.add_collection(artist).
@@ -590,6 +690,77 @@ class SimpleCenterlineGraph(networkx.classes.graph.Graph):
             )
         # Create the artist
         artist = mpl_toolkits.mplot3d.art3d.Line3DCollection(edge_coordinates, **kwargs)
+        return artist
+    
+    def get_node_artist_3d_circles(self, n_sides: int=8, prevalence: float=1.0, **kwargs) -> mpl_toolkits.mplot3d.art3d.Line3DCollection:
+        """Gets the node artist for 3D plotting as a mpl_toolkits.mplot3d.art3d.Line3DCollection.
+        The artist is a collection of 3d planar circles in the 3d space, centered in nodes positions, with their normal
+        direction being the direction towards the next node(s). 
+        Add it to the axes with ax_3d.add_collection(artist).
+
+        This function, due to the heavy use of self.get_flow_direction(), is quite slow.
+
+        Parameters
+        ----------
+        n_sides : int, optional
+            The number of sides to use for each circle (approximation of circle), by default 8.
+        prevalence : float, optional
+            The prevalence of the circles, by default 1.0. The probability that a node will have a circle drawn around it.
+        **kwargs
+            Arbitrary keyword arguments to be passed to the mpl_toolkits.mplot3d.art3d.Line3DCollection constructor.
+
+        Returns
+        -------
+        mpl_toolkits.mplot3d.art3d.Line3DCollection
+            The node artist for 3D plotting.
+        """
+        # Manage prevalence
+        prevalence_vector = []
+        c_ = 0
+        for i in range(len(self.nodes)):
+            if c_*prevalence >= 1:
+                c_ = 0
+                prevalence_vector.append(True)
+            else:
+                prevalence_vector.append(False)
+                c_ += 1
+        nodes_to_process = [n for i, n in enumerate(self.nodes) if prevalence_vector[i]]
+        # Get the node coordinates
+        node_coordinates = [numpy.array([self.nodes[n]["x"], self.nodes[n]["y"], self.nodes[n]["z"]]) for n in nodes_to_process]
+        node_radii = [self.nodes[n]["r"] for n in nodes_to_process]
+        node_directions = [self.get_flow_direction(n) for n in nodes_to_process]
+        thetas = numpy.linspace(0, 2*numpy.pi, n_sides+1)
+        thetas = numpy.append(thetas, thetas[0])
+        circles_2d_list = [
+            [
+                [nr*numpy.cos(th), nr*numpy.sin(th), 0.0]
+                for th in thetas
+            ]
+            for nr in node_radii
+        ] # n, 8 (num_of_sides), 3 (3d coordinates)
+        # now, we transform this circle from the reference frame of the node (with z axis being the direction towards the next node)
+        # to the global reference frame
+        mid_point = numpy.mean(node_coordinates, axis=0).flatten()
+        circles_list_3d = numpy.zeros_like(circles_2d_list, dtype="float") # n, 8, 3
+        for i, (nc, nd, c2d) in enumerate(zip(node_coordinates, node_directions, circles_2d_list)):
+            for j, p in enumerate(c2d):
+                p_np = numpy.array(p)
+                x, y, z = get_orthogonal_axes(
+                    point=nc,
+                    normal=nd,
+                    x_axis_suggestion=mid_point,
+                    suggestion_type="point"
+                )
+                p_proj = transform_from_reference_frame(
+                    origin=nc,
+                    x_versor=x,
+                    y_versor=y,
+                    z_versor=z,
+                    point=p_np
+                )
+                circles_list_3d[i, j, :] = p_proj[:]
+        # Create the artist
+        artist = mpl_toolkits.mplot3d.art3d.Line3DCollection(circles_list_3d, **kwargs)
         return artist
 
 
@@ -723,7 +894,6 @@ if __name__ == "__main__":
         draw_simple_centerlines_graph_2d(reampled_graph)
 
     # Convert to 3D Slicer open curve
-
     if 0:
         from ..utils.slicer import convert_graph_to_3dslicer_fiducials, convert_graph_to_3dslicer_opencurve
         # Graph
@@ -773,23 +943,78 @@ if __name__ == "__main__":
                 affine_transformation_matrix=affine_asoca
             )
 
-    # Get 2d and artists
-    if 0:
+    # Get 2d and 3d artists
+    if 1:
         import matplotlib.pyplot as plt
+        import time
         fig = plt.figure()
         # 2d
         ax = fig.add_subplot(121)
+        time_ = time.time()
         artist = g_.get_edge_artist_2d()
+        print(f"Elapsed: {time.time()-time_} seconds. (get_edge_artist_2d)")
+        ax.add_collection(artist)
+        time_ = time.time()
+        artist = g_.get_node_artist_2d_circles(
+            n_sides=20,
+            prevalence=0.02,
+            linewidths=0.2,
+            alpha=0.2
+        )
+        print(f"Elapsed: {time.time()-time_} seconds. (get_node_artist_2d_circles)")
         ax.add_collection(artist)
         ax.autoscale()
         ax.margins(0.1)
+        ax.set_aspect('equal', 'box')
         # 3d
         ax = fig.add_subplot(122, projection='3d')
+        time_ = time.time()
         artist = g_.get_edge_artist_3d()
+        print(f"Elapsed: {time.time()-time_} seconds. (get_edge_artist_3d)")
         ax.add_collection(artist)
-        ax.autoscale()
-        ax.margins(0.1)
+        time_ = time.time()
+        artist = g_.get_node_artist_3d_circles(
+            n_sides=12,
+            prevalence=0.005,
+            linewidths=0.3,
+            alpha=0.2
+        )
+        print(f"Elapsed: {time.time()-time_} seconds. (get_node_artist_3d_circles)")
+        ax.add_collection(artist)
+        node_positions = numpy.array([[g_.nodes[n]["x"], g_.nodes[n]["y"], g_.nodes[n]["z"]] for n in g_.nodes])
+        ax.set(xlim=[node_positions[:,0].min()-1, node_positions[:,0].max()+1], ylim=[node_positions[:,1].min()-1, node_positions[:,1].max()+1], zlim=[node_positions[:,2].min()-1, node_positions[:,2].max()+1])
         plt.show()
+    
+    # Get next node direction
+    if 0:
+        import matplotlib.pyplot as plt
+        test_dict_ = {}
+        test_dict_["ostium"] = list(g_.get_coronary_ostia_node_id())
+        test_dict_["endpoint"] = [n for n in g_.nodes if g_.nodes[n]["topology"] == ArteryNodeTopology.ENDPOINT]
+        test_dict_["intersection"] = [n for n in g_.nodes if g_.nodes[n]["topology"] == ArteryNodeTopology.INTERSECTION]
+        others = [n for n in g_.nodes if g_.nodes[n]["topology"] == ArteryNodeTopology.SEGMENT]
+        random.shuffle(others)
+        test_dict_["segment"] = others[:5]
+        for k, list_of_nodes in test_dict_.items():
+            for node_id in list_of_nodes:
+                print(f"Node {node_id} ({k})")
+                fig = plt.figure()
+                ax = fig.add_subplot(111, projection='3d')
+                ax.set_title(k)
+                artist = g_.get_edge_artist_3d()
+                ax.add_collection(artist)
+                node_position = numpy.array([g_.nodes[node_id]["x"], g_.nodes[node_id]["y"], g_.nodes[node_id]["z"]])
+                ax.scatter(*node_position, c="b")
+                direction = g_.get_flow_direction(node_id)
+                ax.quiver(
+                    g_.nodes[node_id]["x"], g_.nodes[node_id]["y"], g_.nodes[node_id]["z"], 
+                    direction[0], direction[1], direction[2], 
+                    length=3, normalize=True, color="r"
+                )
+                ax.set(xlim=[node_position[0]-1, node_position[0]+1], ylim=[node_position[1]-1, node_position[1]+1], zlim=[node_position[2]-1, node_position[2]+1])
+                plt.show()
+
+
 
     
 
